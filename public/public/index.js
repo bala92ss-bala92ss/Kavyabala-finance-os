73dbd21b7bf9b4fa63e404bc8947db7274783aa8
bala92ss-bala92ss <67442567+bala92ss-bala92ss@users.noreply.github.com>
2026-08-09 23:31:00 +0530

    index.js

---
+++ public/public/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FinanceApp from './FinanceApp';
+import './FinanceApp.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <FinanceApp />
+  </React.StrictMode>
+);
